bb6c0f3010073953839295f643732b7007a34a7f
feyzsharaf <feyzullayevsharafx1@protonmail.com>
2025-03-04 14:39:04 +0400

    initial ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,64 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./page.module.scss";
+import { ChefHat, CookingPot } from "lucide-react";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <div className={styles.page__title}>
+        <div className={styles.logo}>
+          <ChefHat />
+          <div>Remy</div>
+        </div>
+      </div>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className={styles.form_section}>
+          <div className={styles.title}>
+            Generate a Recipe 
+            <br />
+            <span>
+              For make excellent culinary examples
+            </span>
+          </div>
+          <div className={styles.option_section}>
+            <div className={styles.ingredient_choose}>
+              <div>
+                Choose Ingredients
+              </div>
+              <div>
+                <input 
+                  type="text" 
+                  placeholder="Enter Ingredients"
+                  className={styles.ingredient_input}
+                />
+              </div>
+            </div>
+            <div className={styles.dietary_prefrence}>
+              <div>
+                Dietary Preference
+              </div>
+              <div>
+                <select name="" id="" className={styles.dietary_select}>
+                  <option value="none">None</option>
+                  <option value="vegan">Vegan</option>
+                  <option value="vegatarian">Vegetarian</option>
+                  <option value="gluten-free">Gluten-Free</option>
+                  <option value="dietary-free">Dietary-Free</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <button className={styles.gen_button}>
+            Generate Recipe
+          </button>
+        </div>
+        
+        <div className={styles.recipe_section}>
+          <div className={styles.placeholder}>
+            <CookingPot/>
+            <div>AI-generated recipe will appear here</div>
+          </div> 
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
